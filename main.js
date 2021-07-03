263b5b688a3dbc03d1bae9c456635015893b2875
anto0526 <antojun0526@gmail.com>
2021-07-03 18:41:45 +0900

    Naver UI

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-window.onload = () => {
-    alert("hihihihi");
-};
+const passId = "anto26";
+const passPw = "1q2w3e4r";
+
+function clickOkButton() {
+    const id = document.getElementById("inputId").value;
+    const pw = document.getElementById("inputPw").value;
+
+    if(passId == id && passPw == pw){
+        alert("pass");
+    }
+    else{
+        alert("no");
+    }
+}
